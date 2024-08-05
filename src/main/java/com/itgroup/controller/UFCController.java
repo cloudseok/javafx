581790948856be893ade91d91f4d73def19cd510
cloudseok <seokun1229@gmail.com>
2024-08-05 11:45:23 +0900

    ict 개인프로젝트(4번째 수정)

--- src/main/java/com/itgroup/controller/UFCController.java
+++ src/main/java/com/itgroup/controller/UFCController.java
@@ -78,7 +78,7 @@ public class UFCController implements Initializable {
         this.hostServices = hostServices;
     }
     @FXML
-    public void onImageClick(javafx.scene.input.MouseEvent event) {  // 이미지별 링크 참조
+    public void onImageClick(javafx.scene.input.MouseEvent event) {  // 이미지별 링크 참조 asdf
         String url = "";
         ImageView clickedImageView = (ImageView) event.getSource();
 
